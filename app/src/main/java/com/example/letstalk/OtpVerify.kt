082d3df81e7b45d11271f0b4273a79c687a05a7f
Shubham Gupta <hutesjk@gmail.com>
2021-03-02 10:07:31 -0800

    0.1 init first commit

---
+++ app/src/main/java/com/example/letstalk/OtpVerify.kt
@@ -0,0 +1,61 @@
+package com.example.letstalk
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.letstalk.databinding.ActivityOtpVerifyBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
+
+class OtpVerify : AppCompatActivity() {
+    lateinit var binding: ActivityOtpVerifyBinding
+
+    lateinit var mAuth: FirebaseAuth
+    lateinit var mCallbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+    lateinit var verificationId: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otp_verify)
+
+        binding = ActivityOtpVerifyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val number = intent.getStringExtra("NUMBER")
+        binding.tvNumber.setText(number)
+
+        mAuth = FirebaseAuth.getInstance()
+        mCallbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            override fun onVerificationCompleted(p0: PhoneAuthCredential) {
+
+            }
+
+            override fun onVerificationFailed(p0: FirebaseException) {
+                Toast.makeText(
+                    this@OtpVerify,
+                    "Verification Failed" + p0,
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+
+            override fun onCodeSent(p0: String, p1: PhoneAuthProvider.ForceResendingToken) {
+                verificationId = p0
+            }
+
+        }
+        val phoneNumber = "+91" + (number)
+        val options = PhoneAuthOptions.newBuilder(mAuth)
+            .setPhoneNumber(phoneNumber)
+            .setActivity(this)
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setCallbacks(mCallbacks)
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+
+    }
+}
---
+++ app/src/main/java/com/example/letstalk/PhoneAuthActivity.kt
@@ -0,0 +1,36 @@
+package com.example.letstalk
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.letstalk.databinding.ActivityPhoneAuthBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
+
+class PhoneAuthActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityPhoneAuthBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityPhoneAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.submit.setOnClickListener {
+            val number = binding.etPhone.text.toString()
+            val intent = Intent(this, OtpVerify::class.java)
+            intent.putExtra("NUMBER", number)
+            startActivity(intent)
+            sendCodetoPhoneNumber(number)
+        }
+    }
+
+    private fun sendCodetoPhoneNumber(phone: String) {
+
+    }
+}
